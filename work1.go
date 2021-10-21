fb4b45bbb5a5bc396fd4b2f0813a8e9acbb9ca37
张琦凯 <92740923+1938076874@users.noreply.github.com>
2021-10-21 22:54:27 +0800

    Add files via upload

---
+++ work1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func name(ch chan string) {
+	for i := 1; i <= 10; i++ {
+		ch <- "张三"
+		ch <- "李四"
+		ch <- "王五"
+		ch <- "赵六"
+		a := <-ch
+		b := <-ch
+		c := <-ch
+		d := <-ch
+		fmt.Printf("%v\n%v\n%v\n%v\n", a, b, c, d)
+	}
+}
+
+func main() {
+	intChan := make(chan string, 10)
+	go name(intChan)
+	time.Sleep(10 * time.Second)
+}
